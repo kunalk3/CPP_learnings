b3f872d235b629a06013e8505919d5f197cb2e8f
Kunal K <kunalkolhe333@gmail.com>
2020-12-19 23:41:43 +0530

    Added 3 files

---
+++ factory_variadic.cpp
@@ -0,0 +1,35 @@
+/**
+ * ---------------------------------------------------------------------------------------
+ * Author  : Kunal K.
+ * Title   : Factory method for classes having constructors
+ * Purpose : To demonstrate the concepts of design patterns
+ * Version : 1.0
+ * ---------------------------------------------------------------------------------------
+**/
+
+// Factory method for classes having constructors that take an arbitary number of parameters
+
+#include <iostream>
+#include <memory>
+
+class Factory {
+  public:
+    template <typename T, typename... Params>
+    static auto create(Params... params) {
+        return std::make_unique<T>(params...);
+    }
+};
+
+struct Foo {
+    Foo(int){};
+};
+
+struct Bar {
+    Bar(bool, double){};
+};
+
+int main() {
+    std::shared_ptr<Foo> foo = Factory::create<Foo>(42);
+    auto bar = Factory::create<Bar>(true, 42.5);
+}
+
