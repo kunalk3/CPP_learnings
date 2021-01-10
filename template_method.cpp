a4f146f7545a03d2589b509870fbc1a3f1148c5b
Kunal K <41562231+kunalk3@users.noreply.github.com>
2021-01-10 21:40:27 +0530

    Added 4 cpp files

---
+++ template_method.cpp
@@ -0,0 +1,35 @@
+/**
+ * ---------------------------------------------------------------------------------------
+ * Author  : Kunal K.
+ * Title   : Template method design pattern
+ * Purpose : To demonstrate the concepts of design patterns
+ * Version : 1.0
+ * ---------------------------------------------------------------------------------------
+**/
+
+// Template method design pattern via the Non-Virtual Interface idiom (NVI)
+
+#include <iostream>
+#include <memory>
+
+class Base {
+    virtual void f() { std::cout << "Base::f()\n"; }
+
+  public:
+    void call() {
+        std::cout << "Before calling f()\n";
+        f(); // run-time dispatch
+        std::cout << "After calling f()\n";
+    }
+    virtual ~Base() = default;
+};
+
+class Derived : public Base {
+    // change behaviour
+    void f() override { std::cout << "Derived::f()\n"; }
+};
+
+int main() {
+    std::unique_ptr<Base> pBase{std::make_unique<Derived>()};
+    pBase->call();
+}
